a9f8fedce2e2e5fc59225f715e322bb388e1b36b
MTri-D <minhtri14158@gmail.com>
2023-10-16 00:03:45 +0700

    first

---
+++ app/src/main/java/com/example/caroproject/SecondFragment.java
@@ -0,0 +1,113 @@
+package com.example.caroproject;
+
+import static android.content.Context.MODE_PRIVATE;
+import static android.database.sqlite.SQLiteDatabase.openDatabase;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecondFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecondFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SecondFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SecondFragment newInstance(String param1, String param2) {
+        SecondFragment fragment = new SecondFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private Button btnReset, btnSignUpSecond;
+    private String myData= "MyPreferences";
+    private EditText edtUsername, edtPassword, edtRetype;
+    private SQLiteDatabase myDatabase;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        btnReset=view.findViewById(R.id.btnResetSecond);
+        btnSignUpSecond=view.findViewById(R.id.btnSignUpSecond);
+        edtPassword=view.findViewById(R.id.edtPasswordSecond);
+        edtRetype=view.findViewById(R.id.edtRetypeSecond);
+        edtUsername=view.findViewById(R.id.edtUsernameSecond);
+        btnSignUpSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = edtPassword.getText().toString().trim();
+                String retypePassword = edtRetype.getText().toString().trim();
+                String username = edtRetype.getText().toString().trim();
+                if (username.equals("")==false && password.equals("") ==false && retypePassword.equals("")==false) {
+                    if (password.equals(retypePassword)) {
+                        // Mật khẩu nhập lại khớp với mật khẩu ban đầu
+                        Toast.makeText(requireContext(), "Registration successful!", Toast.LENGTH_SHORT).show();
+                        NavController navController = Navigation.findNavController(v);
+                        navController.navigate(R.id.action_secondFragment_to_firstFragment);
+                    } else {
+                        // Mật khẩu không khớp
+                        Toast.makeText(requireContext(), "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(requireContext(), "Must to enter full information", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtUsername.setText("");
+                edtPassword.setText("");
+                edtRetype.setText("");
+            }
+        });
+        return view;
+    }
+}
