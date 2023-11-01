7e9cb1a070fe4f0356845d274bfd10758c51eca0
LT129 <phamviet12092002@gmail.com>
2023-11-01 13:42:00 +0700

    updateLogin

--- app/src/main/java/com/example/caroproject/MainActivity.java
+++ app/src/main/java/com/example/caroproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_first);
+        setContentView(R.layout.activity_main);
         getWindow().setBackgroundDrawableResource(R.drawable.background_1);
     }
 
